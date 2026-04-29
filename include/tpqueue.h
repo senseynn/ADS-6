247477e211e586cc9c405c01e1e358cc0d21335c
senseynn <163774648+senseynn@users.noreply.github.com>
2026-04-29 08:38:22 +0300

    тест1

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,54 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
-
 struct SYM {
   char ch;
   int prior;
+};
+
+template<typename T>
+class TPQueue {
+ private:
+    struct Node {
+        T data;
+        Node* next;
+        explicit Node(const T& value) : data(value), next(nullptr) {}
+    };
+
+    Node* head;
+
+ public:
+    TPQueue() : head(nullptr) {}
+    ~TPQueue() {
+        while (head != nullptr) {
+            Node* tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+    }
+
+void push(const T& item) {
+        Node* nNode = new Node(item);
+if (!head || item.prior > head->data.prior) {
+            nNode->next = head;
+            head = nNode;
+        } else {
+            Node* current = head;
+            while (current->next && current->next->data.prior >= item.prior) {
+                current = current->next;
+            }
+            nNode->next = current->next;
+            current->next = nNode;
+        }
+    }
+
+T pop() {
+      Node* temp = head;
+      T result = head->data;
+      head = head->next;
+      delete temp;
+      return result;
+    }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
